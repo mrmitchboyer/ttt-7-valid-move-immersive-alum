2a4c5144c8d9119f1337eeb647fd02d0f998db7a
mrmitchboyer <mitch@mitchboyer.com>
2017-11-09 10:55:53 -0500

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  cord = board[index]
+  position_taken?(cord) && index < 8
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(cord)
+  cord == " " || cord == "" || cord == nil
+end
